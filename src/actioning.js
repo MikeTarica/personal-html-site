ba68ce291f4e47f47e3772d8021d350b74762da5
Mike Tarica <mike.tarica@me.com>
2022-03-08 21:30:00 +0200

    java script add

--- src/actioning.js
+++ src/actioning.js
@@ -1,7 +1,10 @@
-(
-
-)(
-    (
-
-    )
-)
+function myFunction() {
+    let text;
+    let person = prompt("Please Enter your name:", "name");
+    if (person == null || person == "") {
+        text = "User cancncelled the propmt.";
+    } else {
+        text = "Hello" + person + "!  haw re you today?";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
