1b51348e164813a5ff34cf8d9eea71d3d8710416
Mike Tarica <mike.tarica@me.com>
2022-03-22 22:33:46 +0200

    java script

--- presonal-site/src/JS/actioning.js
+++ presonal-site/src/JS/actioning.js
@@ -1,8 +1,34 @@
-// function myFunction() {
-//     var txt;
-//     if (confirm("Press a button!")) {
-//         txt = "You pressed OK!";
-//     } else {
-//         txt = "You pressed Cancel!";
-//     }
-//     document.getElementById("demo").innerHTML = txt;
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function myFunction() {
+    document.getElementById("name").innerHTML = "Hello Dolly!";
+    document.getElementById("answer").innerHTML = "How are you?";
+}
+
+function myFunction() {
+    document.getElementById("myHeader").innerHTML = "Javascript Comments";
+    document.getElementById("myParagraph").innerHTML = "Javascript Comments";
+}
+
+function getInputValue() {
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("myInput").value;
+
+    // Displaying the value
+    document.getElementById("myHeader").innerHTML = "Welcome";
+    document.getElementById("myParagraph").innerHTML = (inputVal);
+}
+var x;
+
+var name = prompt("Please enter your Name and Surname", "Harry Potter");
+var gender = prompt("Please enter your gender", "male or female");
+var male;
+var female;
+
+if (gender == male) {
+    alert("Welcome, MR. " + name);
+} else if (gender == female) {
+    alert("Welcome, MS. " + name);
+}
